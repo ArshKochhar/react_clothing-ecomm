6dc7c522295bd2562da7a551ca6f9b8699250f85
Arsh Kochhar <arshkochhar15@gmail.com>
2021-01-28 14:51:53 -0500

    added more components and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
-import HomePage from "./homepage.component.jsx";
+
+import HomePage from "../src/page/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <HomePage />
     </div>
   );
 }
