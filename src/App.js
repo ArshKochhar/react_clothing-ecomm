098b001c9f925493812b86e9a47f8c5bac6dfab1
Arsh Kochhar <arshkochhar15@gmail.com>
2021-01-29 11:19:44 -0500

    developed sign in component and added styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import ShopPage from "../src/page/shop/shop.component";
 import HomePage from "../src/page/homepage/homepage.component";
 import Header from "../src/components/header/header.component";
+import SignInAndSignUpPage from "./page/sign in and sign up page/sign-in-and-sign-up.component";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
