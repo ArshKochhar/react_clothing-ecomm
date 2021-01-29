cab49c3ab8712dd252c30a18b2003a41cab79d16
Arsh Kochhar <arshkochhar15@gmail.com>
2021-01-28 19:28:27 -0500

    added a header to the application that allows to switch tabs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import ShopPage from "../src/page/shop/shop.component";
 import HomePage from "../src/page/homepage/homepage.component";
+import Header from "../src/components/header/header.component";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
