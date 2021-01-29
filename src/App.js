30c096e72817b4365cd8f8b67ba900867fd3a132
Arsh Kochhar <arshkochhar15@gmail.com>
2021-01-28 19:09:41 -0500

    updating the app with shop page of collection prview and item component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
-
+import ShopPage from "../src/page/shop/shop.component";
 import HomePage from "../src/page/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/hats" component={HatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
